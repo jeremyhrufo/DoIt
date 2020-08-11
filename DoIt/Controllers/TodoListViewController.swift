f9f111c370681d948753c1e4687cdafbc398d6f8
Jeremy Rufo <jeremyhrufo@gmail.com>
2020-08-11 08:36:39 -0400

    Some cleanup and updating title in items area with the category name

--- DoIt/Controllers/TodoListViewController.swift
+++ DoIt/Controllers/TodoListViewController.swift
@@ -13,26 +13,25 @@ class TodoListViewController: UITableViewController {
     //MARK: - Members
     let realm = try! Realm()
     var doItItems: Results<Item>?
-    
     var selectedCategory: Category? {
-        didSet {
-            // Once 'selectedCategory' is set with a value, retrieve our data
+        didSet { // Once 'selectedCategory' is set with a value, retrieve our data
             retrieveData()
+            title = selectedCategory?.name
         }
     }
-    
+
     //MARK: - Normal functionality
     @IBAction func addButtonPressed (_ sender: UIBarButtonItem) {
         let alert = UIAlertController(title: "Add New Item", message: nil, preferredStyle: .alert)
-        
+
         // Add a cancel button
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        
+
         // Create a text field to be used to extend the scope of the alert text field
         alert.addTextField(configurationHandler: { alertTextField in
             alertTextField.placeholder = "Enter item description here..."
         })
-        
+
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
             // Grab the text out of the alert text field and save it
             if let text = alert.textFields?.first?.text {
@@ -48,7 +47,7 @@ class TodoListViewController: UITableViewController {
                 self.reloadTable()
             }
         }))
-        
+
         present(alert, animated: true, completion: nil)
     }
 }
@@ -58,11 +57,11 @@ extension TodoListViewController {
     override func tableView (_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.doItItems?.count ?? 1
     }
-    
+
     override func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Get the reusable cell
         let cell = tableView.dequeueReusableCell(withIdentifier: K.itemReusableCellName, for: indexPath)
-        
+
         if let item = self.doItItems?[indexPath.row] {
             // Set default label and accessory and return the cell
             cell.textLabel?.text = item.title
@@ -70,7 +69,7 @@ extension TodoListViewController {
         } else {
             cell.textLabel?.text = "No Items Added"
         }
-        
+
         return cell
     }
 }
@@ -100,34 +99,11 @@ extension TodoListViewController {
 
 //MARK: - Storage functions
 extension TodoListViewController {
-    // Retrieve our data with passed in NSFetchRequest
-    /*func retrieveData (with request: NSFetchRequest<Item> = Item.fetchRequest()) {
-     
-     // Here is our category predicate
-     var predicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
-     
-     // If our request already have a predicate, add them both to a compound predicate
-     // I don't know, yet, if I can add a compound predicate to another one
-     if request.predicate != nil {
-     predicate = NSCompoundPredicate(type: NSCompoundPredicate.LogicalType.and, subpredicates: [predicate, request.predicate!])
-     }
-     
-     request.predicate = predicate
-     
-     do {
-     self.itemArray = try context.fetch(request)
-     } catch {
-     print("Error fetching data from context, \(error)")
-     }
-     
-     // reload our table
-     self.tableView?.reloadData()
-     }*/
     func retrieveData() {
         doItItems = selectedCategory?.items.sorted(byKeyPath: "dateCreated", ascending: true)
         self.reloadTable()
     }
-    
+
     // Save our data in core data
     func saveData (item: Item) {
         do {
@@ -156,7 +132,7 @@ extension TodoListViewController: UISearchBarDelegate {
             .sorted(byKeyPath: "dateCreated", ascending: true)
         self.reloadTable()
     }
-    
+
     func searchBar (_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             retrieveData()
